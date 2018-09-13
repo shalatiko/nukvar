3ccd4a6a89dacf7c5e0901a16a4a1a366a34c456
eldarmagrafta <eldarmagrafta@eldars-mbp>
2018-09-14 01:16:54 +0300

    first change

--- MAKO/src/Bdokme.java
+++ MAKO/src/Bdokme.java
@@ -3,7 +3,11 @@ public class Bdokme {
 
 	public static void main(String[] args) {
 		System.out.println("ddddddd5g55555");
-
+		System.out.println(num11(55));
+	}
+	
+	public static double num11(int x) {
+		return 11.6;
 	}
 
 }
